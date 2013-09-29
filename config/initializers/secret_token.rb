dc1604cdbbd6c5564414d73b82a2b2953a0c25a1
yunixon <yunixon@gmail.com>
2013-09-29 13:18:09 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitbats::Application.config.secret_key_base = 'a8323a0bf1d6abf3a20a5bd741a677984c5bc898aa0ebffaeaf0d855079e8ad3783cf38d2e031cb313a06d2a9ffbc3c8e2bbf4d4d22852e396c0bffc2781daea'
